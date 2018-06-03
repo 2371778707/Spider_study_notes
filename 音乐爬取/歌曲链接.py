b55270358de6ccf1dc09a5f4a50fd51e6903d8e0
2371778707 <2371778707@qq.com>
2018-06-03 20:37:51 +0800

    QQ自定义群发消息

---
+++ 音乐爬取/歌曲链接.py
@@ -0,0 +1,31 @@
+import urllib.request
+import re
+res=urllib.request.urlopen("http://www.kuwo.cn/bang/index")
+html=res.read().decode('utf-8')
+
+#网页源码只有一个<ul></ul>标签  ？可用可不用
+r1 = r'<ul class="listMusic">[\s\S]*</ul>'
+r2 = r'<div class="name">[\s\S]*?</div>'
+r3 = r'<a href="([\s\S]*)" target="_blank">[\s\S]*</a>'
+
+#只会返回一个，列表长度为1
+result1 = re.findall(r1,html)
+
+#非贪婪模式
+result2= re.findall(r2,result1[0])
+
+result3 = []
+
+for i in range(len(result2)):
+
+    result3.append(re.findall(r3,result2[i]))
+
+with open('音乐地址.txt','w',encoding='utf-8') as f:
+
+    for i in range(len(result3)):
+
+        f.write(''.join(result3[i])+'\n')
+
+    f.close()
+
+
