2ad6128484a4c2c79651347fd4fbee578c05fdad
2371778707 <2371778707@qq.com>
2018-06-06 20:41:03 +0800

    王者荣耀

--- 王者荣耀英雄图片爬取/wzry.py
+++ 王者荣耀英雄图片爬取/wzry.py
@@ -25,9 +25,13 @@ def change_link(links):
 
 def get_hero_html(links):
     image_links =[]
+
+
     for link in links:
 
         r1 = r'<div class="zk-con1 zk-con" style="background:url\(\'([\S\s]*?)\'\) center 0">'
+
+
         res = request.urlopen(link)
         #解码
         html = res.read().decode('gbk')
@@ -37,25 +41,52 @@ def get_hero_html(links):
 
         image_links.append(image_link)
 
+
     return image_links
 
-def download_image(image_links):
+
+def skin_link(image_links):
+
+    all_image_links=[]
+
+
+    for i in range(len(image_links)):
+
+        for x in range(7):
+
+            image_link = image_links[i][0:-5] + str(x + 1) + '.jpg'
+
+            try:
+                res = request.urlopen(image_link)
+
+            except Exception:
+
+                break
+
+
+            if res.code == 200:
+
+
+                all_image_links.append(image_link)
+
+
+
+    return  all_image_links
+
+def download_image(all_image_links):
 
     path = os.path.join(os.getcwd(),'王者荣耀图片文件')
     if  not os.path.exists(path):
 
         os.mkdir(path)
 
-    for i in range(len(image_links)):
+    for i in range(len(all_image_links)):
+        print('第' + str(i + 1) + '张下载中......')
+        urlretrieve(all_image_links[i], '%s.jpg' % (path+'/'+str(i+1)))
+        print('第' + str(i + 1) + '张下载完成\n')
 
-        time.sleep(0.5)
-        urlretrieve(image_links[i],'%s.jpg' %  (os.path.join(path,'第'+str(i+1)+'张图')))
-
-        print('第'+str(i+1)+'张图片正在下载中......')
-
-        if i+1 == len(image_links):
-
-            print('下载完成！！！')
+        if i+1 == len(all_image_links):
+            print('全部下载完成！！！')
 
 
 
@@ -66,8 +97,10 @@ def main():
     html =  get_html(url)
     links =re_html(html)
     links = change_link(links)
-    image_links = get_hero_html(links)
-    download_image(image_links)
+    image_links= get_hero_html(links)
+    all_image_links = skin_link(image_links)
+    download_image(all_image_links)
+
 
 if __name__ == '__main__':
     main()
