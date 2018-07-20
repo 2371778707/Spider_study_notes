4ed1e8d270b40d60a40b6076b73acbe028793aa0
2371778707 <2371778707@qq.com>
2018-07-20 14:04:14 +0800

    歌词爬去

--- 音乐爬取/download_lrc.py
+++ 音乐爬取/download_lrc.py
@@ -36,48 +36,57 @@ for i in range(len(music_link_list)):
 
     result2 =re.findall(r2,html)
 
-    print('第'+str(i+1)+'首歌词下载完成！')
+    if result1==[]:
 
-    try :
-
-        list1 = result2[0].split('>')
-    except Exception :
-        print("对不起，网络错误！请重新运行！")
-
-    for  a  in range(len(list1)):
-
-         if a%2==0 :
-             pass
-         else:
-             list2 = list1[a].split('<')
-             music_lrc.append(list2[0])
-
-
-
-    if not os.path.exists(path1):
-
-       os.mkdir(path1)
-       # 处理名字中有/的
-    include_ = ''.join(result1[0]).split('/')
-
-    if len(include_) == 0:
-
-        with open(path1+'/'+''.join(result1[0])+'.txt','w') as f:
-
-
-              for music in music_lrc:
-
-                 f.write(''.join(music)+'\n')
-
-              f.close()
+        pass
 
     else:
+        print(result1[0]+"歌词下载完成！")
 
-        with open(path1+'/'+''.join(include_)+'.txt','w') as f:
+        try :
 
-            for music in music_lrc:
+            list1 = result2[0].split('>')
+        except Exception :
 
-                f.write(''.join(music) + '\n')
+            print("对不起，网络错误！请重新运行！")
 
-            f.close()
+        for  a  in range(len(list1)):
+
+             if a%2==0 :
+                 pass
+             else:
+
+                 list2 = list1[a].split('<')
+                 music_lrc.append(list2[0])
+
+        if not os.path.exists(path1):
+
+           os.mkdir(path1)
+
+           # 处理名字中有/的
+
+
+        include_ = ''.join(result1[0]).split('/')
+
+
+        if len(include_) == 0:
+
+            with open(path1+'/'+''.join(result1[0])+'.txt','w') as f:
+
+
+                  for music in music_lrc:
+
+                     f.write(''.join(music)+'\n')
+
+                  f.close()
+
+        else:
+
+            with open(path1+'/'+''.join(include_)+'.txt','w') as f:
+
+                for music in music_lrc:
+
+                    f.write(''.join(music) + '\n')
+
+                f.close()
 
