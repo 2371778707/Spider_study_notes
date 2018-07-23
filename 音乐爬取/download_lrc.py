16bf730d3b65b7ca38449ba7ebea910c20f81218
2371778707 <2371778707@qq.com>
2018-07-23 23:03:52 +0800

    歌词爬取

--- 音乐爬取/download_lrc.py
+++ 音乐爬取/download_lrc.py
@@ -1,25 +1,26 @@
 from urllib.request import *
 import re
 import os
+# 导入歌词链接
 import music_link
 
 r1 = r'<p id="lrcName">([\s\S]*?)</p>'
-
 r2 = r'<div id="llrcId" style="overflow-x: hidden;overflow-y: hidden;">[\s\S]*?([\s\S]*?)</div>'
+# 调用link（）
 
 music_link.link()
 
-path = os.path.join(os.getcwd(),"音乐地址.txt")
+path = os.path.join(os.getcwd(), "音乐地址.txt")
 
 path1 = os.path.join(os.getcwd(), '歌词')
 
-with open(path,'r',encoding='utf-8') as f:
-    music_link_str=f.read()
+with open(path, 'r', encoding='utf-8') as f:
+    music_link_str = f.read()
     f.close()
 
-music_link_list =music_link_str.split('\n')
+music_link_list = music_link_str.split('\n')
 
-music_link_list = music_link_list[0:len(music_link_list)-1]
+music_link_list = music_link_list[0:len(music_link_list) - 1]
 
 
 for i in range(len(music_link_list)):
@@ -30,52 +31,49 @@ for i in range(len(music_link_list)):
 
     html = res.read().decode('utf-8')
 
-    result1 =re.findall(r1,html)
+    result1 = re.findall(r1, html)
 
-    result2 =re.findall(r2,html)
+    result2 = re.findall(r2, html)
 
-    if result1==[]:
+    if result1 == []:
 
         pass
 
     else:
-        print(result1[0]+"歌词下载完成！")
-
-        try :
-
+        print("歌曲名称：" + result1[0])
+        try:
             list1 = result2[0].split('>')
 
-        except Exception :
+        except Exception:
 
-            print("对不起，网络错误！请重新运行！")
+            pass
 
-        for  a  in range(len(list1)):
+        for a in range(len(list1)):
 
-             if a%2==0 :
-                 pass
-             else:
+            if a % 2 == 0:
+                pass
+            else:
 
-                 list2 = list1[a].split('<')
-                 music_lrc.append(list2[0])
+                list2 = list1[a].split('<')
+                music_lrc.append(list2[0])
 
         if not os.path.exists(path1):
 
-           os.mkdir(path1)
+            os.mkdir(path1)
 
-           # 处理名字中有/的
+            # 处理名字中有/，|，\\的
 
+        IrlName = ''.join(result1[0]).replace("/", "-")
 
-        IrlName =''.join(result1[0]).replace("/","-")
+        IrlName1 = IrlName.replace("|", "-")
 
+        IrlName2 = IrlName1.replace("\\", "-")
 
-
-
-        with open(path1+'/'+IrlName+'.txt','w') as f:
-
-
-              for music in music_lrc:
-
-                 f.write(''.join(music)+'\n')
-
-              f.close()
-
+        with open(path1 + '/' + IrlName2 + '.txt', 'w', encoding="utf-8") as f:
+            try:
+                f.write(IrlName2 + '\n')
+                for music in music_lrc:
+                    f.write(music + '\n')
+                f.close()
+            except:
+                pass
--- 音乐爬取/music_link.py
+++ 音乐爬取/music_link.py
@@ -1,31 +1,32 @@
 import urllib.request
 import re
 
+
 def link():
 
-    res=urllib.request.urlopen("http://www.kuwo.cn/bang/index")
-    html=res.read().decode('utf-8')
+    res = urllib.request.urlopen("http://www.kuwo.cn/bang/index")
+    html = res.read().decode('utf-8')
 
-    #网页源码只有一个<ul></ul>标签  ？可用可不用
+    # 网页源码只有一个<ul></ul>标签 可用可不用
     r1 = r'<ul class="listMusic">[\s\S]*</ul>'
     r2 = r'<div class="name">[\s\S]*?</div>'
     r3 = r'<a href="([\s\S]*)" target="_blank">[\s\S]*</a>'
 
-    #只会返回一个，列表长度为1
-    result1 = re.findall(r1,html)
-    #非贪婪模式
-    result2= re.findall(r2,result1[0])
+    # 只会返回一个，列表长度为1
+    result1 = re.findall(r1, html)
+    # 非贪婪模式
+    result2 = re.findall(r2, result1[0])
 
     result3 = []
 
     for i in range(len(result2)):
 
-        result3.append(re.findall(r3,result2[i]))
+        result3.append(re.findall(r3, result2[i]))
 
-    with open('音乐地址.txt','w',encoding='utf-8') as f:
+    with open('音乐地址.txt', 'w', encoding='utf-8') as f:
 
         for i in range(len(result3)):
 
-            f.write(''.join(result3[i])+'\n')
+            f.write(''.join(result3[i]) + '\n')
 
         f.close()
